73f0b7cc7d3a5e8bff6573881aafecfe4586a973
Purrfect Developer <test@example.com>
2026-07-14 21:14:30 +0530

    SearchInRotatedBinarySearch.java

--- src/searching/BinarySearch/SearchInRotatedBinarySearch.java
+++ src/searching/BinarySearch/SearchInRotatedBinarySearch.java
@@ -65,4 +65,36 @@ public class SearchInRotatedBinarySearch {
         return -1;
     }
 
+    //now this method will check with duplicates
+    static int findPivotwithduplicates(int[] arr){
+        int start=0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            // now these checks are defining whether the elements in start , mid , end and if thats the case then just skip the duplicates
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]){
+                if(arr[start]>arr[start+1]){
+                    return start;
+                }
+                start++;
+                if(arr[end]<arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            } else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]) {
+                start = mid+1;
+                
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
 }
