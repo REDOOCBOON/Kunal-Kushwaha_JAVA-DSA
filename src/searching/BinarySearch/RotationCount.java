ca7497eb1f0eb7a40c34f26ba5a686c827a48731
Purrfect Developer <test@example.com>
2026-07-15 16:51:23 +0530

    SearchInRotatedBinarySearch.java

---
+++ src/searching/BinarySearch/RotationCount.java
@@ -0,0 +1,69 @@
+package searching.BinarySearch;
+
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(countRotations(arr));
+    }
+
+    private static int countRotations(int[] arr) {
+        int pivot = findPivot(arr);
+//        if(pivot == -1){
+//            // array is not rotated
+//            return 0;
+//        }
+        return pivot +1;
+    }
+    // use this method for non duplicates
+    static int findPivot(int[] arr){
+        int start=0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid]<=arr[start]) {
+                end = mid - 1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    //now this method will check with duplicates
+    static int findPivotwithduplicates(int[] arr){
+        int start=0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            // now these checks are defining whether the elements in start , mid , end and if thats the case then just skip the duplicates
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]){
+                if(arr[start]>arr[start+1]){
+                    return start;
+                }
+                start++;
+                if(arr[end]<arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            } else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]) {
+                start = mid+1;
+
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/searching/BinarySearch/SplitArrayLargestSum.java
@@ -0,0 +1,10 @@
+package searching.BinarySearch;
+
+
+//https://leetcode.com/problems/split-array-largest-sum/description/
+
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+        
+    }
+}
