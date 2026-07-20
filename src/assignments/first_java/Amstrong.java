6b2dbc40f7376805ee932136f75e6b970e21cc78
Ujjwal Thakur <ujjwal3rd@gmail.com>
2026-07-20 20:52:21 +0530

    armstrong number code done

--- src/assignments/first_java/Amstrong.java
+++ src/assignments/first_java/Amstrong.java
@@ -6,35 +6,55 @@ import java.util.Scanner;
 public class Amstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the range of two numbers:");
+        System.out.println("Enter the range:");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        int ans = findAmstrong(num1,num2);
-        if(ans != -1){
-            System.out.println(ans);
-        }else{
-            System.out.println("No amstrong number between them");
-        }
-    }
 
-    private static int iterate(int num1, int num2) {
-        int start = num1;
-        int end = num2;
-        for(int i=num1;i<num2;i++){
-            if(findAmstrong(i)){
-                System.out.println(i+"is an armstrong");
+        for(int i = num1;i<num2;i++){
+            if(isArmstrong(i)){
+                System.out.println(i);
             }
         }
+        sc.close();
+
 
     }
-    private static boolean findAmstrong(int num){
-        int cubednum=0;
-        while (num > 0) {
-            int rem = num%10;
-            cubednum = cubednum+(rem*rem*rem);
-            num=num/10;
-
+//    public static int iterate(int num1,int num2){
+//
+//        return -1;
+//    }
+    public static boolean isArmstrong(int num){
+        if(num<0){
+            num =num*-1;
         }
-        return false;
+        int ognum = num;
+        int digits = count(num);
+        int rem;
+        int sum=0;
+        while(num>0){
+            rem = num%10;
+            sum = sum + (int)Math.pow(rem,digits);
+            num = num/10;
+        }
+
+        return sum == ognum;
     }
+    public static int count(int num){
+
+        int count =0;
+        if(num==0){
+            count = 1;
+        }
+        if(num<0){
+            num=num*-1;
+        }
+
+        while(num>0){
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
+
+
 }
