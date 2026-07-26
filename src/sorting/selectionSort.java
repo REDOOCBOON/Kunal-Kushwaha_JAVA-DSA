152c103e478cede5708ab8f317e2fb9153eefc22
Purrfect Developer <test@example.com>
2026-07-26 23:54:08 +0530

    assignments

---
+++ src/sorting/selectionSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr = {34,34,45,-213,542,122,1,0};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            //find the max item in the remaining array and swap with correct index
+            int last =  arr.length-i-1;
+            int maxIndex = getMax(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+    private static int getMax(int[] arr, int start, int end){
+        int max = start;
+        for (int i = start; i <=end ; i++) {
+            if(arr[max]<arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
