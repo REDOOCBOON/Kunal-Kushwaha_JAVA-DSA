20587d769967226382d8d1a4652f8c41756ac81c
Purrfect Developer <test@example.com>
2026-07-30 20:59:07 +0530

    assignments

---
+++ src/sorting/Duplicate.java
@@ -0,0 +1,32 @@
+package sorting;
+
+class Duplicate {
+    public int findDuplicate(int[] nums) {
+
+        int i =0    ;
+        while(i<nums.length){
+            if(nums[i] != i+1){
+                int correctIndex = nums[i] - 1;
+                if(nums[i] != nums[correctIndex]){
+                    swap(nums , i , correctIndex);
+                }else{
+                    return nums[i];
+                }
+            }else{
+                i++;
+            }
+        }
+        return -1;
+
+        
+    }
+
+
+
+
+    void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
---
+++ src/sorting/FindAllDuplicates.java
@@ -0,0 +1,34 @@
+package sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/2086682075/
+public class FindAllDuplicates {
+    public List<Integer> findDuplicates(int[] arr) {
+        int i=0;
+        while(i<arr.length){
+            int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]){
+                swap(arr,i,correctIndex);
+            }else{
+                i++;
+            }
+
+        }
+
+        //find all missing
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index+1){
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+
+    }
+    static void swap(int[] arr,int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
